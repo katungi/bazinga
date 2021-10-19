2eb5a6c22ea0d52a06ecb3962854488244aa7b7b
Daniel Dennis <dankatdennis@gmail.com>
2021-10-19 08:39:29 +0300

    - added get

---
+++ android/app/src/main/kotlin/com/katungi/bazinga/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.katungi.bazinga
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Bazinga',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+    );
+  }
+}
